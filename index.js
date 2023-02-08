9404d676c3f93ef28120ff309892c86b590057f4
2005balaji <balajisr000@gmail.com>
2023-02-08 18:21:40 +0530

    new

--- index.js
+++ index.js
@@ -5,9 +5,14 @@ const fs = require("fs");
 const { exec } = require("child_process");
 
 exec( "cd  build-tools && ./aapt2 dump badging test.apk ", (a, b, c) => {
-  fs.writeFileSync("apk.txt", b);
 
-  var txt = b;
+  var str = b;
+  var res = str.replace(/\s+/g, "");
+  // console.log(res);
+
+  fs.writeFileSync("apk.txt", res);
+
+  var txt = res;
 
   let data = txt;
 
@@ -16,16 +21,31 @@ exec( "cd  build-tools && ./aapt2 dump badging test.apk ", (a, b, c) => {
 
   let startIndex = data.indexOf("label=") + 7
 
-  let endIndex = data.indexOf("' icon='");
+  let endIndex = data.indexOf("'icon='");
 
   if (startIndex !== -1 && endIndex !== -1) {
     let name = data.substring(startIndex, endIndex);
-    console.log(name);
+    console.log("App Name : "+name);
   } else {
     console.log("Name not found in data.");
   }
 
 }
 
+
+{
+
+  let startIndex = data.indexOf("sdkVersion:'") + 12
+
+  let endIndex = data.indexOf("'targetSdkVersion:");
+
+  if (startIndex !== -1 && endIndex !== -1) {
+    let name = data.substring(startIndex, endIndex);
+    console.log("Min SDK Version : "+name);
+  } else {
+    console.log("Name not found in data.");
+  }
+
+}
 
 });
