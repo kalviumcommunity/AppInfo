e5028f046825c9ae13a1ec1df1edb1504e06f644
2005balaji <balajisr000@gmail.com>
2023-03-03 11:14:41 +0530

    New Details

--- Readme.md
+++ Readme.md
@@ -1,1 +1,7 @@
 This branch has the logic of getting the details out from APK 
+
+versionCode and versionName: These two values indicate the version of the app. versionCode is an integer value that's used by Android to differentiate between different versions of the same app, and versionName is a human-readable string that provides more detail about the version.
+compileSdkVersion and targetSdkVersion: These values indicate the Android SDK versions that the app was compiled against and designed to run on, respectively.
+uses-permission: These tags list the permissions that the app requires in order to run. For example, android.permission.INTERNET indicates that the app needs to be able to access the internet, and android.permission.WRITE_EXTERNAL_STORAGE indicates that it needs permission to write to external storage.
+application-label: These tags indicate the localized name of the app in various languages.
+uses-feature: These tags list the hardware and software features that the app requires in order to run. For example, android.hardware.faketouch indicates that the app requires a touch screen.
--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const fs = require("fs");
 
 /// executing shell commands
-
 const { exec } = require("child_process");
 
-exec( "cd  build-tools && ./aapt2 dump badging test.apk ", (a, b, c) => {
-
-  var str = b;
+exec("cd uploads && ./aapt2 dump badging test.apk ", (a, b, c) => {
+  var newtext = b + "textended";
+  var str = newtext;
   var res = str.replace(/\s+/g, "");
-  // console.log(res);
 
   fs.writeFileSync("apk.txt", res);
 
@@ -16,36 +14,168 @@ exec( "cd  build-tools && ./aapt2 dump badging test.apk ", (a, b, c) => {
 
   let data = txt;
 
+  {
+    let startIndex = data.indexOf("label=") + 7;
+
+    let endIndex = data.indexOf("'icon='");
+
+    if (startIndex !== -1 && endIndex !== -1) {
+      let name = data.substring(startIndex, endIndex);
+      console.log("App Name : " + name);
+    } else {
+      console.log("Name not found in data.");
+    }
+  }
 
   {
+    let startIndex = data.indexOf("sdkVersion:'") + 13;
 
-  let startIndex = data.indexOf("label=") + 7
+    let endIndex = data.indexOf("'targetSdkVersion:");
 
-  let endIndex = data.indexOf("'icon='");
-
-  if (startIndex !== -1 && endIndex !== -1) {
-    let name = data.substring(startIndex, endIndex);
-    console.log("App Name : "+name);
-  } else {
-    console.log("Name not found in data.");
+    if (startIndex !== -1 && endIndex !== -1) {
+      let name = data.substring(startIndex, endIndex);
+      console.log("Min SDK Version : " + name);
+    } else {
+      console.log("Name not found in data.");
+    }
   }
 
-}
+  {
+    let startIndex = data.indexOf("versionName='") + 13;
 
+    let endIndex = data.indexOf("'platformBuildV");
 
-{
-
-  let startIndex = data.indexOf("sdkVersion:'") + 12
-
-  let endIndex = data.indexOf("'targetSdkVersion:");
-
-  if (startIndex !== -1 && endIndex !== -1) {
-    let name = data.substring(startIndex, endIndex);
-    console.log("Min SDK Version : "+name);
-  } else {
-    console.log("Name not found in data.");
+    if (startIndex !== -1 && endIndex !== -1) {
+      let name = data.substring(startIndex, endIndex);
+      console.log("versionName : " + name);
+    } else {
+      console.log("Name not found in data.");
+    }
   }
 
-}
+  {
+    let startIndex = data.indexOf("'versionCode='") + 14;
 
+    let endIndex = data.indexOf("'versionName");
+
+    if (startIndex !== -1 && endIndex !== -1) {
+      let name = data.substring(startIndex, endIndex);
+      console.log("versionCode : " + name);
+    } else {
+      console.log("Name not found in data.");
+    }
+  }
+
+  {
+    let startIndex = data.indexOf("package:name='") + 14;
+
+    let endIndex = data.indexOf("'versionCode");
+
+    if (startIndex !== -1 && endIndex !== -1) {
+      let name = data.substring(startIndex, endIndex);
+      console.log("Package Name : " + name);
+    } else {
+      console.log("Name not found in data.");
+    }
+  }
+
+  {
+    let startIndex = data.indexOf("targetSdkVersion:'") + 18;
+
+    let endIndex = data.indexOf("'uses-permis");
+
+    if (startIndex !== -1 && endIndex !== -1) {
+      let name = data.substring(startIndex, endIndex);
+      console.log("Target SdkVersion : " + name);
+    } else {
+      console.log("Name not found in data.");
+    }
+  }
+
+  {
+    let startIndex = data.indexOf("supports-screens:") + 17;
+
+    let endIndex = data.indexOf("supports-any-");
+
+    if (startIndex !== -1 && endIndex !== -1) {
+      let name = data.substring(startIndex, endIndex);
+      console.log("Support Screen sizes: " + name);
+    } else {
+      console.log("Name not found in data.");
+    }
+  }
+
+  {
+    let startIndex = data.indexOf("u'densities:'") + 17;
+
+    let endIndex = data.indexOf("textended");
+
+    if (startIndex !== -1 && endIndex !== -1) {
+      let name = data.substring(startIndex, endIndex);
+      console.log("Supported Screen Densities: " + name);
+    } else {
+      console.log("Name not found in data.");
+    }
+  }
+
+  {
+    let startIndex = data.indexOf("feature-group:label=''") + 22;
+
+    let endIndex = data.indexOf("other-activities");
+
+    if (startIndex !== -1 && endIndex !== -1) {
+      let str = data.substring(startIndex, endIndex);
+
+      let newStr = "";
+
+      for (let i = 0; i < str.length; i++) {
+        if (str[i] === "'") {
+          newStr += str[i];
+          while (i + 1 < str.length && str[i + 1] !== "'") {
+            newStr += str[i + 1];
+            i++;
+          }
+          newStr += "',";
+        } else {
+          newStr += str[i];
+        }
+      }
+
+      console.log("Features : " + newStr);
+    } else {
+      console.log("Name not found in data.");
+    }
+  }
+
+  /// executing shell commands
+
+  {
+    exec("cd uploads && ./aapt2 dump permissions test.apk ", (a, values, c) => {
+      console.log("Permissions : " + values);
+    });
+  }
+
+  {
+    let startIndex = data.indexOf("locales:") + 8;
+
+    let endIndex = data.indexOf("densities");
+
+    if (startIndex !== -1 && endIndex !== -1) {
+      let name = data.substring(startIndex, endIndex);
+      console.log("Languages " + name);
+    } else {
+      console.log("Name not found in data.");
+    }
+  }
+
+  /// executing shell commands
+
+  {
+    exec(
+      "cd uploads && ./keytool -printcert -jarfile test.apk ",
+      (a, values, c) => {
+        console.log("Signature : " + values);
+      }
+    );
+  }
 });
