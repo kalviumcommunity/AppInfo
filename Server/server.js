fcabe65b3605f46bc6218efaca30ae496a73c864
2005balaji <balajisr000@gmail.com>
2023-03-03 12:50:45 +0530

    server details

--- Server/server.js
+++ Server/server.js
@@ -21,7 +21,6 @@ app.post("/upload", upload.single("file"), (req, res) => {
   console.log("File uploaded successfully!");
 
   if (AppName != undefined) {
-    
     // ///////////////////////////////////////////////////////
     console.log("Executing cd uploads && ./aapt2 dump badging " + AppName);
 
@@ -32,11 +31,9 @@ app.post("/upload", upload.single("file"), (req, res) => {
 
       fs.writeFileSync("apk.txt", res);
 
-      var txt = res;
+      var data = res;
 
-      let data = txt;
-
-      {
+      function AppName() {
         let startIndex = data.indexOf("label=") + 7;
 
         let endIndex = data.indexOf("'icon='");
@@ -49,8 +46,8 @@ app.post("/upload", upload.single("file"), (req, res) => {
         }
       }
 
-      {
-        let startIndex = data.indexOf("sdkVersion:'") + 12;
+      function MinSDKVersion() {
+        let startIndex = data.indexOf("sdkVersion:'") + 13;
 
         let endIndex = data.indexOf("'targetSdkVersion:");
 
@@ -58,19 +55,166 @@ app.post("/upload", upload.single("file"), (req, res) => {
           let name = data.substring(startIndex, endIndex);
           console.log("Min SDK Version : " + name);
         } else {
-          console.log("Min SDK not found in data.");
+          console.log("Name not found in data.");
         }
       }
+
+      function versionName() {
+        let startIndex = data.indexOf("versionName='") + 13;
+
+        let endIndex = data.indexOf("'platformBuildV");
+
+        if (startIndex !== -1 && endIndex !== -1) {
+          let name = data.substring(startIndex, endIndex);
+          console.log("versionName : " + name);
+        } else {
+          console.log("Name not found in data.");
+        }
+      }
+
+      function versionCode() {
+        let startIndex = data.indexOf("'versionCode='") + 14;
+
+        let endIndex = data.indexOf("'versionName");
+
+        if (startIndex !== -1 && endIndex !== -1) {
+          let name = data.substring(startIndex, endIndex);
+          console.log("versionCode : " + name);
+        } else {
+          console.log("Name not found in data.");
+        }
+      }
+
+      function PackageName() {
+        let startIndex = data.indexOf("package:name='") + 14;
+
+        let endIndex = data.indexOf("'versionCode");
+
+        if (startIndex !== -1 && endIndex !== -1) {
+          let name = data.substring(startIndex, endIndex);
+          console.log("Package Name : " + name);
+        } else {
+          console.log("Name not found in data.");
+        }
+      }
+
+      function TargetSdkVersion() {
+        let startIndex = data.indexOf("targetSdkVersion:'") + 18;
+
+        let endIndex = data.indexOf("'uses-permis");
+
+        if (startIndex !== -1 && endIndex !== -1) {
+          let name = data.substring(startIndex, endIndex);
+          console.log("Target SdkVersion : " + name);
+        } else {
+          console.log("Name not found in data.");
+        }
+      }
+
+      function SupportScreensizes() {
+        let startIndex = data.indexOf("supports-screens:") + 17;
+
+        let endIndex = data.indexOf("supports-any-");
+
+        if (startIndex !== -1 && endIndex !== -1) {
+          let name = data.substring(startIndex, endIndex);
+          console.log("Support Screen sizes: " + name);
+        } else {
+          console.log("Name not found in data.");
+        }
+      }
+
+      function SupportedScreenDensities() {
+        let startIndex = data.indexOf("u'densities:'") + 17;
+
+        let endIndex = data.indexOf("textended");
+
+        if (startIndex !== -1 && endIndex !== -1) {
+          let name = data.substring(startIndex, endIndex);
+          console.log("Supported Screen Densities: " + name);
+        } else {
+          console.log("Name not found in data.");
+        }
+      }
+
+      function Features() {
+        let startIndex = data.indexOf("feature-group:label=''") + 22;
+
+        let endIndex = data.indexOf("other-activities");
+
+        if (startIndex !== -1 && endIndex !== -1) {
+          let str = data.substring(startIndex, endIndex);
+
+          let newStr = "";
+
+          for (let i = 0; i < str.length; i++) {
+            if (str[i] === "'") {
+              newStr += str[i];
+              while (i + 1 < str.length && str[i + 1] !== "'") {
+                newStr += str[i + 1];
+                i++;
+              }
+              newStr += "',";
+            } else {
+              newStr += str[i];
+            }
+          }
+
+          console.log("Features : " + newStr);
+        } else {
+          console.log("Name not found in data.");
+        }
+      }
+
+      function Permissions() {
+        exec(
+          "cd uploads && ./aapt2 dump permissions test.apk ",
+          (a, values, c) => {
+            console.log("Permissions : " + values);
+            Languages();
+          }
+        );
+      }
+
+      function Languages() {
+        let startIndex = data.indexOf("locales:") + 8;
+
+        let endIndex = data.indexOf("densities");
+
+        if (startIndex !== -1 && endIndex !== -1) {
+          let name = data.substring(startIndex, endIndex);
+          console.log("Languages " + name);
+        } else {
+          console.log("Name not found in data.");
+        }
+      }
+
+      function Signature() {
+        exec(
+          "cd uploads && ./keytool -printcert -jarfile test.apk ",
+          (a, values, c) => {
+            console.log("Signature : " + values);
+          }
+        );
+      }
+
+      AppName();
+      MinSDKVersion();
+      versionName();
+      versionCode();
+      PackageName();
+      TargetSdkVersion();
+      SupportScreensizes();
+      SupportedScreenDensities();
+      Features();
+      Permissions();
+      Signature();
     });
 
-
-
-    
-//removing the APK file
-exec("cd uploads && rm "+AppName, (a, b, c) => {
-  console.log("File deleted "+ AppName)
-})
-
+    //removing the APK file
+    exec("cd uploads && rm " + AppName, (a, b, c) => {
+      console.log("File deleted " + AppName);
+    });
   } else {
   }
   // delete file named 'sample.txt'
@@ -83,4 +227,3 @@ const port = 3005; // or any port you prefer
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
-
