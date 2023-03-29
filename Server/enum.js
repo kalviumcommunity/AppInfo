98af335b35c758ccac4f28e16673f205468fab1c
2005balaji <balajisr000@gmail.com>
2023-03-29 11:41:32 +0530

    updated enum file

--- Server/enum.js
+++ Server/enum.js
@@ -1,6 +1,5 @@
 const commands = Object.freeze({
   aaptDump: "cd resources && ./aapt2 dump badging ",
-  mongoUrl: "mongodb+srv://balajis:hcsnTgKf5YhQ6oGw@cluster1.uzi21wt.mongodb.net/apkdetails",
   deleter:"cd resources && rm ",
   aaptPermission:"cd resources && ./aapt2 dump permissions ",
   keytoolDump:"cd resources && keytool -printcert -jarfile "
