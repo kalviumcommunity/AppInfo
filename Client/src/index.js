fbc93f4b8cee3768b61844c7a0d0e1032ae5b3e6
2005balaji <balajisr000@gmail.com>
2023-03-30 09:43:54 +0530

    Misc. Bugs

--- Client/src/index.js
+++
@@ -1,9 +0,0 @@
-  import React, { createContext } from "react";
-import ReactDOM from "react-dom/client";
-import App from './app'
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  
-<App/>
-);
