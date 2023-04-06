264230cfc4bdcdac6670b18117a42aad2b36f0b4
2005balaji <balajisr000@gmail.com>
2023-04-06 11:58:36 +0530

    added images.js for clean coding

---
+++ Client/src/components/Images/images.js
@@ -0,0 +1,17 @@
+// images.js
+const images = [
+  {
+    src: 'path/to/image1.jpg',
+    alt: 'Image 1',
+  },
+  {
+    src: 'path/to/image2.jpg',
+    alt: 'Image 2',
+  },
+  {
+    src: 'path/to/image3.jpg',
+    alt: 'Image 3',
+  },
+];
+
+export default images;
