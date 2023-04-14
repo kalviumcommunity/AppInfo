92bdefb259ec1a1a288d68a93af139dc64cf195e
2005balaji <balajisr000@gmail.com>
2023-04-14 10:45:00 +0530

    removed un necc. line

--- Client/src/components/Uploadbox/Uploadbox.jsx
+++ Client/src/components/Uploadbox/Uploadbox.jsx
@@ -7,7 +7,6 @@ import { useAuth0 } from "@auth0/auth0-react";
 import "./Uploadbox.css";
 import { VscCloudUpload } from "react-icons/vsc";
 import { useDropzone } from "react-dropzone";
-import LoadingText from "../loading/loading";
 
 
 
